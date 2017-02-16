6dfb607bd03c10d499cf2e011d3c257f6ced28b2
Maxim Biro <nurupo.contributions@gmail.com>
2017-02-16 05:11:19 -0500

    Install runtime modules into bin directory
    Runtime modules, such as executables and shared libraries should be
    installed into "bin" instead of "lib".

--- cmake/ModulePackage.cmake
+++ cmake/ModulePackage.cmake
@@ -61,12 +61,18 @@ function(add_module lib)
       VERSION ${SOVERSION}
       SOVERSION ${SOVERSION_MAJOR}
     )
-    install(TARGETS ${lib}_shared DESTINATION "lib")
+    install(TARGETS ${lib}_shared
+      RUNTIME DESTINATION bin
+      LIBRARY DESTINATION lib
+      ARCHIVE DESTINATION lib)
   endif()
   if(ENABLE_STATIC)
     add_library(${lib}_static STATIC ${ARGN})
     set_target_properties(${lib}_static PROPERTIES OUTPUT_NAME ${lib})
-    install(TARGETS ${lib}_static DESTINATION "lib")
+    install(TARGETS ${lib}_static
+      RUNTIME DESTINATION bin
+      LIBRARY DESTINATION lib
+      ARCHIVE DESTINATION lib)
   endif()
 endfunction()
 
