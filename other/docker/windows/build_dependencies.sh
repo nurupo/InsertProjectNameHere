ead9dd12f8b023f22ef2f6ff0fd57706d034a28c
Restyled.io <commits@restyled.io>
2020-05-27 16:24:56 +0000

    Restyled by shellharden

--- other/docker/windows/build_dependencies.sh
+++ other/docker/windows/build_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # disable on Cygwin otherwise some builds fail
-if [ "${CROSS_COMPILE}" = "true" ]; then
+if [ "$CROSS_COMPILE" = "true" ]; then
     set -e -x
 fi
 
@@ -11,61 +11,61 @@ build()
 {
     ARCH=${1}
 
-    echo "Building for ${ARCH} architecture"
+    echo "Building for $ARCH architecture"
 
     # set some things
-    WINDOWS_TOOLCHAIN=${ARCH}-w64-mingw32
+    WINDOWS_TOOLCHAIN=$ARCH-w64-mingw32
 
     # prefix that we will copy to the user
-    PREFIX_DIR="/root/prefix/${ARCH}"
-    rm -rf "${PREFIX_DIR}"
-    mkdir -p "${PREFIX_DIR}"
+    PREFIX_DIR="/root/prefix/$ARCH"
+    rm -rf "$PREFIX_DIR"
+    mkdir -p "$PREFIX_DIR"
 
-    export MAKEFLAGS=j$(nproc)
+    export MAKEFLAGS=j"$(nproc)"
     export CFLAGS=-O3
 
     cd /tmp
     rm -rf /tmp/*
 
     echo
-    echo "=== Building Sodium ${VERSION_SODIUM} ${ARCH} ==="
-    git clone --depth=1 --branch="${VERSION_SODIUM}" https://github.com/jedisct1/libsodium
+    echo "=== Building Sodium $VERSION_SODIUM $ARCH ==="
+    git clone --depth=1 --branch="$VERSION_SODIUM" https://github.com/jedisct1/libsodium
     cd libsodium
     ./autogen.sh
-    ./configure --host="${WINDOWS_TOOLCHAIN}" --prefix="${PREFIX_DIR}" --disable-shared --enable-static
+    ./configure --host="$WINDOWS_TOOLCHAIN" --prefix="$PREFIX_DIR" --disable-shared --enable-static
     make
     make install
     cd ..
 
     echo
-    echo "=== Building Opus ${VERSION_OPUS} ${ARCH} ==="
-    if [ "${CROSS_COMPILE}" = "true" ]; then
-        git clone --depth=1 --branch="${VERSION_OPUS}" https://github.com/xiph/opus
+    echo "=== Building Opus $VERSION_OPUS $ARCH ==="
+    if [ "$CROSS_COMPILE" = "true" ]; then
+        git clone --depth=1 --branch="$VERSION_OPUS" https://github.com/xiph/opus
         cd opus
         ./autogen.sh
     else
         # autogen.sh failed on Cygwin due to ltmain.sh symlink
         VERSION_OPUS="${VERSION_OPUS#?}" # remove first 'v'
-        curl "https://archive.mozilla.org/pub/opus/opus-${VERSION_OPUS}.tar.gz" -o opus.tar.gz
+        curl "https://archive.mozilla.org/pub/opus/opus-$VERSION_OPUS.tar.gz" -o opus.tar.gz
         tar xzf opus.tar.gz
-        cd "opus-${VERSION_OPUS}"
+        cd "opus-$VERSION_OPUS"
     fi
-    ./configure --host="${WINDOWS_TOOLCHAIN}" --prefix="${PREFIX_DIR}" --disable-extra-programs --disable-doc --disable-shared --enable-static
+    ./configure --host="$WINDOWS_TOOLCHAIN" --prefix="$PREFIX_DIR" --disable-extra-programs --disable-doc --disable-shared --enable-static
     make
     make install
     cd ..
 
     echo
-    echo "=== Building VPX ${VERSION_VPX} ${ARCH} ==="
+    echo "=== Building VPX $VERSION_VPX $ARCH ==="
     LIB_VPX_TARGET=""
-    if [ "${ARCH}" = "i686" ]; then
+    if [ "$ARCH" = "i686" ]; then
         LIB_VPX_TARGET=x86-win32-gcc
     else
         LIB_VPX_TARGET=x86_64-win64-gcc
     fi
-    git clone --depth=1 --branch="${VERSION_VPX}" https://github.com/webmproject/libvpx
+    git clone --depth=1 --branch="$VERSION_VPX" https://github.com/webmproject/libvpx
     cd libvpx
-    CROSS="${WINDOWS_TOOLCHAIN}"- ./configure --target="${LIB_VPX_TARGET}" --prefix="${PREFIX_DIR}" --disable-examples --disable-unit-tests --disable-shared --enable-static
+    CROSS="$WINDOWS_TOOLCHAIN"- ./configure --target="$LIB_VPX_TARGET" --prefix="$PREFIX_DIR" --disable-examples --disable-unit-tests --disable-shared --enable-static
     make
     make install
     cd ..
@@ -73,11 +73,11 @@ build()
     rm -rf /tmp/*
 }
 
-if [ "${SUPPORT_ARCH_i686}" = "true" ]; then
+if [ "$SUPPORT_ARCH_i686" = "true" ]; then
     build i686
 fi
 
-if [ "${SUPPORT_ARCH_x86_64}" = "true" ]; then
+if [ "$SUPPORT_ARCH_x86_64" = "true" ]; then
     build x86_64
 fi
 
--- other/docker/windows/build_toxcore.sh
+++ other/docker/windows/build_toxcore.sh
@@ -8,81 +8,81 @@ build()
 {
     ARCH=${1}
 
-    echo "Building for ${ARCH} architecture"
+    echo "Building for $ARCH architecture"
 
     # set some things
 
-    WINDOWS_TOOLCHAIN=${ARCH}-w64-mingw32
+    WINDOWS_TOOLCHAIN=$ARCH-w64-mingw32
 
     # toxcore dependencies that we will copy to the user for static build of toxcore (e.g. vpx, opus, sodium)
-    DEP_PREFIX_DIR="/root/prefix/${ARCH}"
+    DEP_PREFIX_DIR="/root/prefix/$ARCH"
 
     # toxcore dependencies that user doesn't need in build result
-    EXTRA_DEP_PREFIX_DIR="/root/extra-prefix/${ARCH}"
-    mkdir -p "${EXTRA_DEP_PREFIX_DIR}"
+    EXTRA_DEP_PREFIX_DIR="/root/extra-prefix/$ARCH"
+    mkdir -p "$EXTRA_DEP_PREFIX_DIR"
 
     # where to put the result of this particular build
-    RESULT_PREFIX_DIR="/prefix/${ARCH}"
-    rm -rf "${RESULT_PREFIX_DIR}"
-    mkdir -p "${RESULT_PREFIX_DIR}"
+    RESULT_PREFIX_DIR="/prefix/$ARCH"
+    rm -rf "$RESULT_PREFIX_DIR"
+    mkdir -p "$RESULT_PREFIX_DIR"
 
     rm -rf /tmp/*
 
     # where to install static/shared toxcores before deciding whether they should be copied over to the user
     STATIC_TOXCORE_PREFIX_DIR="/tmp/static_prefix"
     SHARED_TOXCORE_PREFIX_DIR="/tmp/shared_prefix"
-    mkdir -p "${STATIC_TOXCORE_PREFIX_DIR}" "${SHARED_TOXCORE_PREFIX_DIR}"
+    mkdir -p "$STATIC_TOXCORE_PREFIX_DIR" "$SHARED_TOXCORE_PREFIX_DIR"
 
-    export MAKEFLAGS=j$(nproc)
+    export MAKEFLAGS=j"$(nproc)"
     export CFLAGS=-O3
 
     echo
-    echo "=== Building toxcore ${ARCH} ==="
-    export PKG_CONFIG_PATH="${DEP_PREFIX_DIR}/lib/pkgconfig:${EXTRA_DEP_PREFIX_DIR}/lib/pkgconfig"
+    echo "=== Building toxcore $ARCH ==="
+    export PKG_CONFIG_PATH="$DEP_PREFIX_DIR/lib/pkgconfig:$EXTRA_DEP_PREFIX_DIR/lib/pkgconfig"
 
-    if [ "${CROSS_COMPILE}" = "true" ]; then
+    if [ "$CROSS_COMPILE" = "true" ]; then
         TOXCORE_DIR="/toxcore"
     else
         # get Toxcore root
         cd "$( cd "$( dirname -- "$0" )" >/dev/null 2>&1 && pwd )"
         cd ../../../
-        TOXCORE_DIR="$(pwd)"
+        TOXCORE_DIR="$PWD"
     fi
 
-    cp "${TOXCORE_DIR}" /tmp/toxcore -R
+    cp "$TOXCORE_DIR" /tmp/toxcore -R
     cd /tmp/toxcore/build
 
     echo "
         SET(CMAKE_SYSTEM_NAME Windows)
 
-        SET(CMAKE_C_COMPILER   ${WINDOWS_TOOLCHAIN}-gcc)
-        SET(CMAKE_CXX_COMPILER ${WINDOWS_TOOLCHAIN}-g++)
-        SET(CMAKE_RC_COMPILER  ${WINDOWS_TOOLCHAIN}-windres)
+        SET(CMAKE_C_COMPILER   $WINDOWS_TOOLCHAIN-gcc)
+        SET(CMAKE_CXX_COMPILER $WINDOWS_TOOLCHAIN-g++)
+        SET(CMAKE_RC_COMPILER  $WINDOWS_TOOLCHAIN-windres)
 
-        SET(CMAKE_FIND_ROOT_PATH /usr/${WINDOWS_TOOLCHAIN} ${DEP_PREFIX_DIR} ${EXTRA_DEP_PREFIX_DIR})
+        SET(CMAKE_FIND_ROOT_PATH /usr/$WINDOWS_TOOLCHAIN $DEP_PREFIX_DIR $EXTRA_DEP_PREFIX_DIR)
     " > windows_toolchain.cmake
 
-    if [ "${ENABLE_TEST}" = "true" ]; then
+    if [ "$ENABLE_TEST" = "true" ]; then
         echo "SET(CROSSCOMPILING_EMULATOR /usr/bin/wine)" >> windows_toolchain.cmake
     fi
 
     cmake -DCMAKE_TOOLCHAIN_FILE=windows_toolchain.cmake \
-        -DCMAKE_INSTALL_PREFIX="${STATIC_TOXCORE_PREFIX_DIR}" \
+        -DCMAKE_INSTALL_PREFIX="$STATIC_TOXCORE_PREFIX_DIR" \
         -DENABLE_SHARED=OFF \
         -DENABLE_STATIC=ON \
-        -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS}" \
-        -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
-        -DCMAKE_EXE_LINKER_FLAGS="${CMAKE_EXE_LINKER_FLAGS} -fstack-protector" \
-        -DCMAKE_SHARED_LINKER_FLAGS="${CMAKE_SHARED_LINKER_FLAGS}" \
-        ${EXTRA_CMAKE_FLAGS} \
+        -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" \
+        -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+        -DCMAKE_EXE_LINKER_FLAGS="$CMAKE_EXE_LINKER_FLAGS -fstack-protector" \
+        -DCMAKE_SHARED_LINKER_FLAGS="$CMAKE_SHARED_LINKER_FLAGS" \
+        "$EXTRA_CMAKE_FLAGS" \
         ..
-    cmake --build . --target install -- -j$(nproc)
+    cmake --build . --target install -- -j"$(nproc)"
 
-    if [ "${ENABLE_TEST}" = "true" ]; then
+    if [ "$ENABLE_TEST" = "true" ]; then
         rm -rf /root/.wine
 
         # setup wine
-        if [ "${ARCH}" = "i686" ]; then
+        if [ "$ARCH" = "i686" ]; then
             export WINEARCH=win32
         else
             export WINEARCH=win64
@@ -91,90 +91,90 @@ build()
         winecfg
         export CTEST_OUTPUT_ON_FAILURE=1
         # add libgcc_s_sjlj-1.dll libwinpthread-1.dll into PATH env var of wine
-        export WINEPATH=`cd /usr/lib/gcc/${WINDOWS_TOOLCHAIN}/*posix/ ; winepath -w $(pwd)`\;`winepath -w /usr/${WINDOWS_TOOLCHAIN}/lib/`\;`winepath -w ${EXTRA_DEP_PREFIX_DIR}/bin`
+        export WINEPATH="$(cd /usr/lib/gcc/"$WINDOWS_TOOLCHAIN"/*posix/ ; winepath -w "$PWD")"\;"$(winepath -w /usr/"$WINDOWS_TOOLCHAIN"/lib/)"\;"$(winepath -w "$EXTRA_DEP_PREFIX_DIR"/bin)"
 
-        if [ "${ALLOW_TEST_FAILURE}" = "true" ]; then
+        if [ "$ALLOW_TEST_FAILURE" = "true" ]; then
             set +e
         fi
         cmake --build . --target test -- ARGS="-j50"
-        if [ "${ALLOW_TEST_FAILURE}" = "true" ]; then
+        if [ "$ALLOW_TEST_FAILURE" = "true" ]; then
             set -e
         fi
     fi
 
     # move static dependencies
-    cp "${STATIC_TOXCORE_PREFIX_DIR}"/* "${RESULT_PREFIX_DIR}" -R
-    cp "${DEP_PREFIX_DIR}"/* "${RESULT_PREFIX_DIR}" -R
+    cp "$STATIC_TOXCORE_PREFIX_DIR"/* "$RESULT_PREFIX_DIR" -R
+    cp "$DEP_PREFIX_DIR"/* "$RESULT_PREFIX_DIR" -R
 
     # make libtox.dll
-    cd "${SHARED_TOXCORE_PREFIX_DIR}"
-    for archive in ${STATIC_TOXCORE_PREFIX_DIR}/lib/libtox*.a
+    cd "$SHARED_TOXCORE_PREFIX_DIR"
+    for archive in "$STATIC_TOXCORE_PREFIX_DIR"/lib/libtox*.a
     do
-        ${WINDOWS_TOOLCHAIN}-ar xv ${archive}
+        "$WINDOWS_TOOLCHAIN"-ar xv "$archive"
     done
 
-    if [ "${CROSS_COMPILE}" = "true" ]; then
-        LIBWINPTHREAD="/usr/${WINDOWS_TOOLCHAIN}/sys-root/mingw/lib/libwinpthread.a"
+    if [ "$CROSS_COMPILE" = "true" ]; then
+        LIBWINPTHREAD="/usr/$WINDOWS_TOOLCHAIN/sys-root/mingw/lib/libwinpthread.a"
     else
-        LIBWINPTHREAD="/usr/${WINDOWS_TOOLCHAIN}/lib/libwinpthread.a"
+        LIBWINPTHREAD="/usr/$WINDOWS_TOOLCHAIN/lib/libwinpthread.a"
     fi
 
-    ${WINDOWS_TOOLCHAIN}-gcc -Wl,--export-all-symbols \
+    "$WINDOWS_TOOLCHAIN"-gcc -Wl,--export-all-symbols \
                              -Wl,--out-implib=libtox.dll.a \
                              -shared \
                              -o libtox.dll \
                              *.obj \
-                             ${STATIC_TOXCORE_PREFIX_DIR}/lib/*.a \
-                             ${DEP_PREFIX_DIR}/lib/*.a \
-                             "${LIBWINPTHREAD}" \
+                             "$STATIC_TOXCORE_PREFIX_DIR"/lib/*.a \
+                             "$DEP_PREFIX_DIR"/lib/*.a \
+                             "$LIBWINPTHREAD" \
                              -liphlpapi \
                              -lws2_32 \
                              -static-libgcc \
                              -lssp
-    cp libtox.dll.a ${RESULT_PREFIX_DIR}/lib
-    mkdir -p ${RESULT_PREFIX_DIR}/bin
-    cp libtox.dll ${RESULT_PREFIX_DIR}/bin
+    cp libtox.dll.a "$RESULT_PREFIX_DIR"/lib
+    mkdir -p "$RESULT_PREFIX_DIR"/bin
+    cp libtox.dll "$RESULT_PREFIX_DIR"/bin
 
     rm -rf /tmp/*
 
     # remove everything from include directory except tox headers
-    mv ${RESULT_PREFIX_DIR}/include/tox ${RESULT_PREFIX_DIR}/tox
-    rm -rf ${RESULT_PREFIX_DIR}/include/*
-    mv ${RESULT_PREFIX_DIR}/tox ${RESULT_PREFIX_DIR}/include/tox
+    mv "$RESULT_PREFIX_DIR"/include/tox "$RESULT_PREFIX_DIR"/tox
+    rm -rf "$RESULT_PREFIX_DIR"/include/*
+    mv "$RESULT_PREFIX_DIR"/tox "$RESULT_PREFIX_DIR"/include/tox
 
-    sed -i "s|^prefix=.*|prefix=${RESULT_PREFIX_DIR}|g" ${RESULT_PREFIX_DIR}/lib/pkgconfig/*.pc
-    sed -i "s|^libdir=.*|libdir=${RESULT_PREFIX_DIR}/lib|g" ${RESULT_PREFIX_DIR}/lib/*.la
+    sed -i "s|^prefix=.*|prefix=$RESULT_PREFIX_DIR|g" "$RESULT_PREFIX_DIR"/lib/pkgconfig/*.pc
+    sed -i "s|^libdir=.*|libdir=$RESULT_PREFIX_DIR/lib|g" "$RESULT_PREFIX_DIR"/lib/*.la
 }
 
 #=== Test Supported vs. Enabled ===
 
-if [ "${ENABLE_ARCH_i686}" != "true" ] && [ "${ENABLE_ARCH_x86_64}" != "true" ]; then
+if [ "$ENABLE_ARCH_i686" != "true" ] && [ "$ENABLE_ARCH_x86_64" != "true" ]; then
     echo "Error: No architecture specified. Set either ENABLE_ARCH_i686 or ENABLE_ARCH_x86_64 or both."
     exit 1
 fi
 
-if [ "${ENABLE_ARCH_i686}" = "true" ] && [ "${SUPPORT_ARCH_i686}" != "true" ]; then
+if [ "$ENABLE_ARCH_i686" = "true" ] && [ "$SUPPORT_ARCH_i686" != "true" ]; then
     echo "Error: Can't build for i686 architecture because the image was created without SUPPORT_ARCH_i686 set"
     exit 1
 fi
 
-if [ "${ENABLE_ARCH_x86_64}" = "true" ] && [ "${SUPPORT_ARCH_x86_64}" != "true" ]; then
+if [ "$ENABLE_ARCH_x86_64" = "true" ] && [ "$SUPPORT_ARCH_x86_64" != "true" ]; then
     echo "Error: Can't build for x86_64 architecture because the image was created without SUPPORT_ARCH_x86_64 set"
     exit 1
 fi
 
-if [ "${ENABLE_TEST}" = "true" ] && [ "${SUPPORT_TEST}" != "true" ]; then
+if [ "$ENABLE_TEST" = "true" ] && [ "$SUPPORT_TEST" != "true" ]; then
     echo "Error: Can't build with tests because the image was created without SUPPORT_TEST set"
     exit 1
 fi
 
 #=== Build ===
 
-if [ "${ENABLE_ARCH_i686}" = "true" ]; then
+if [ "$ENABLE_ARCH_i686" = "true" ]; then
     build i686
 fi
 
-if [ "${ENABLE_ARCH_x86_64}" = "true" ]; then
+if [ "$ENABLE_ARCH_x86_64" = "true" ]; then
     build x86_64
 fi
 
@@ -186,6 +186,6 @@ echo "Built toxcore successfully!"
 echo
 
 # since we are building as root
-if [ "${CROSS_COMPILE}" = "true" ]; then
+if [ "$CROSS_COMPILE" = "true" ]; then
     chmod 777 /prefix -R
 fi
