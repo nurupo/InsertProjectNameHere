528b5ad2a38bdee46090fc9a470c09db08e4ce02
avoidr <avoidr@posteo.de>
2021-09-29 20:43:46 +0000

    Don't try to prepare video if video bit rate is 0
    This fixes breaking audio calls, if for any reason video init fails.
    
    In my case, I was hitting TOXAV_ERR_ANSWER_CODEC_INITIALIZATION, caused
    by "ABI version mismatch" in `vpx_codec_enc_init()`. The error was fixed
    by rebuilding toxcore, but it shouldn't have come up in an audio-only
    call regardless.

--- toxav/toxav.c
+++ toxav/toxav.c
@@ -1339,7 +1339,7 @@ static bool call_prepare_transmission(ToxAVCall *call)
             goto FAILURE;
         }
     }
-    {   /* Prepare video */
+    if (call->video_bit_rate) { /* Prepare video */
         call->video = vc_new(av->toxav_mono_time, av->m->log, av, call->friend_number, av->vcb, av->vcb_user_data);
 
         if (!call->video) {
