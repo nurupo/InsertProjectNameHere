bf0ed0a94edf0551520f4f0b7796af7e034179aa
Maxim Biro <nurupo.contributions@gmail.com>
2023-12-01 03:59:25 -0500

    Spellcheck tox-bootstrapd

--- other/bootstrap_daemon/src/config.c
+++ other/bootstrap_daemon/src/config.c
@@ -405,7 +405,7 @@ int bootstrap_from_config(const char *cfg_file_path, DHT *dht, int enable_ipv6)
         log_write(LOG_LEVEL_INFO, "Successfully added bootstrap node #%d: %s:%d %s\n", i, bs_address, bs_port, bs_public_key);
 
 next:
-        // config_setting_lookup_string() allocates string inside and doesn't allow us to free it direcly
+        // config_setting_lookup_string() allocates string inside and doesn't allow us to free it directly
         // though it's freed when the element is removed, so we free it right away in order to keep memory
         // consumption minimal
         config_setting_remove_elem(node_list, 0);
